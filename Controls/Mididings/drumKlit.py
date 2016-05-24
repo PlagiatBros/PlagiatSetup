4eacd6f31aa980a4390b722c125d97b244952819
Wladimir Ohrelianov II <woii@SCstage.sebkhachott.net>
2016-05-24 22:42:14 +0200

    Pas de nuances sur clap prex propre

--- Controls/Mididings/drumKlit.py
+++ Controls/Mididings/drumKlit.py
@@ -68,34 +68,11 @@ tapeutape=Output('DKTapeutape',10)
 
 #### Slicing ####
 slicing = PortFilter('DKJeannotIn') >> [
-#    ProgramFilter(1) >> stop, # !!!STOP!!! #
     Filter(NOTE) >> 
     [
         KeyFilter(64) >> NoteOn(64, 127),
         ~KeyFilter(64) >> Pass(),
         ]
-# [
-#         KeyFilter(60) >> [ # Gestion Volume Sample 1
-#             Filter(NOTEON) >> SendOSC(samplesmainport, '/strip/Samples1Dry/Gain/Mute', 1.0) >> Discard(),
-#             Filter(NOTEOFF) >> SendOSC(samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0) >> Discard()
-#             ],
-#         KeyFilter(61) >> [ # Gestion Volume Sample 2
-#             Filter(NOTEON) >> SendOSC(samplesmainport, '/strip/Samples2Dry/Gain/Mute', 1.0) >> Discard(),
-#             Filter(NOTEOFF) >> SendOSC(samplesmainport, '/strip/Samples2Dry/Gain/Mute', 0.0) >> Discard()
-#             ],
-#         KeyFilter(62) >> [ # Gestion Volume Sample 3
-#             Filter(NOTEON) >> SendOSC(samplesmainport, '/strip/Samples3Dry/Gain/Mute', 1.0) >> Discard(),
-#             Filter(NOTEOFF) >> SendOSC(samplesmainport, '/strip/Samples3Dry/Gain/Mute', 0.0) >> Discard()
-#             ],
-#         KeyFilter(63) >> [ # Gestion Volume Sample 4
-#             Filter(NOTEON) >> SendOSC(samplesmainport, '/strip/Samples4Dry/Gain/Mute', 1.0) >> Discard(),
-#             Filter(NOTEOFF) >> SendOSC(samplesmainport, '/strip/Samples4Dry/Gain/Mute', 0.0) >> Discard()
-#             ],
-#         KeyFilter(64) >> [ # Gestion Volume Sample 5
-#             Filter(NOTEON) >> SendOSC(samplesmainport, '/strip/Samples5Dry/Gain/Mute', 1.0) >> Discard(),
-#             Filter(NOTEOFF) >> SendOSC(samplesmainport, '/strip/Samples5Dry/Gain/Mute', 0.0) >> Discard()
-#             ],
-#         ],
     ]
 
 
