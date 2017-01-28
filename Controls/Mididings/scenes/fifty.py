7855ec743815c11957a2cba5e623bada6947059d
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-01-28 10:10:43 +0100

    fifty: bite maked

--- Controls/Mididings/scenes/fifty.py
+++ Controls/Mididings/scenes/fifty.py
@@ -12,7 +12,7 @@ from mididings.extra.osc import SendOSC
 fifty = [
     [orl, jeannot] >> Filter(PROGRAM) >> Ctrl(0, 2) >> tapeutapecontrol,
     [orl, jeannot] >> ProgramFilter(1) >> stop, # !!!STOP!!! #
-    orl >> ProgramFilter(2) >> [ # Couplet - Bouton 2
+    orl >> ProgramFilter(2) >> [ # Intro (fin du sample) - Bouton 2
         Program(65) >> cseqtrigger,
         [
             SendOSC(slport, '/set', 'eighth_per_cycle', 64),
@@ -51,32 +51,41 @@ fifty = [
             vxjeannotgars_on,
             vxjeannotmeuf_off,
             vxjeannotdisint_off,
+            vxjeannotvocode_off,
+
 
             ] >> Discard()
         ],
-    orl >> ProgramFilter(3) >> [ # Pont Refrain - Bouton 3
+    orl >> ProgramFilter(3) >> [ # Intro (bouclage basse, cycle complet) - Bouton 3
         Program(66) >> cseqtrigger,
         [
-            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
+            SendOSC(slport, '/set', 'eighth_per_cycle', 64),
             SendOSC(slport, '/set', 'tempo', 117),
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
 
             SendOSC(klickport, '/klick/simple/set_tempo', 117),
-            SendOSC(klickport, '/klick/simple/set_meter', 3, 4),
-            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxx'),
+            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
             SendOSC(klickport, '/klick/metro/start'),
+
+            # Bouclage basse manuel pour l'instant
 
             SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.64925),
             SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.64925),
             SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.32222),
 
-
-            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
-
             SendOscState([
 
-                [samplesmainport, '/strip/Samples3Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/Samples2Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/Samples4Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/Samples5Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/SamplesMunge/Gain/Mute', 0.0],
+
+                [samplesdelaymungeport, '/strip/Samples2/Gain/Gain%20(dB]/unscaled', -7.0],
 
             ]),
+
 
             vxorlgars_on,
             vxorlmeuf_off,
@@ -88,18 +97,20 @@ fifty = [
             vxjeannotgars_on,
             vxjeannotmeuf_off,
             vxjeannotdisint_off,
+            vxjeannotvocode_off,
+
             ] >> Discard()
         ],
-    orl >> ProgramFilter(4) >> [ # Refrain - Bouton 4
+    orl >> ProgramFilter(4) >> [ # Boucle rationnelle - Bouton 4
         Program(67) >> cseqtrigger,
         [
-            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
+            SendOSC(slport, '/set', 'eighth_per_cycle', 64),
             SendOSC(slport, '/set', 'tempo', 117),
             SendOSC(slport, '/sl/-1/hit', 'pause_on'),
 
             SendOSC(klickport, '/klick/simple/set_tempo', 117),
-            SendOSC(klickport, '/klick/simple/set_meter', 3, 4),
-            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxx'),
+            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
             SendOSC(klickport, '/klick/metro/start'),
 
             SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.64925),
@@ -108,9 +119,16 @@ fifty = [
 
             SendOscState([
 
-                [samplesmainport, '/strip/Samples3Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/Samples2Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/Samples4Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/Samples5Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/SamplesMunge/Gain/Mute', 0.0],
+
+                [samplesdelaymungeport, '/strip/Samples2/Gain/Gain%20(dB]/unscaled', -7.0],
 
             ]),
+
 
             vxorlgars_on,
             vxorlmeuf_off,
@@ -122,11 +140,12 @@ fifty = [
             vxjeannotgars_on,
             vxjeannotmeuf_off,
             vxjeannotdisint_off,
+            vxjeannotvocode_off,
 
             ] >> Discard()
         ],
     orl >> ProgramFilter(5) >> [ # Couplet avec boucle - Bouton 5
-        Program(65) >> cseqtrigger,
+        Program(66) >> cseqtrigger,
         [
             SendOSC(slport, '/set', 'eighth_per_cycle', 64),
             SendOSC(slport, '/set', 'tempo', 117),
@@ -168,37 +187,36 @@ fifty = [
             vxjeannotgars_on,
             vxjeannotmeuf_off,
             vxjeannotdisint_off,
+            vxjeannotvocode_off,
 
             ] >> Discard()
         ],
-    orl >> ProgramFilter(6) >> [ # Boucle rationnelle - Bouton 6
+    orl >> ProgramFilter(6) >> [ # Pont Refrain - Bouton 6
+        #TODO son synthé
         Program(68) >> cseqtrigger,
         [
-            SendOSC(slport, '/set', 'eighth_per_cycle', 64),
+            SendOSC(audioseqport, '/Audioseq/Scene/Play', 'fifty_refrain_auto'),
+
+            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
             SendOSC(slport, '/set', 'tempo', 117),
-            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
 
             SendOSC(klickport, '/klick/simple/set_tempo', 117),
-            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
-            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+            SendOSC(klickport, '/klick/simple/set_meter', 3, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxx'),
             SendOSC(klickport, '/klick/metro/start'),
 
             SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.64925),
             SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.64925),
             SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.32222),
 
+
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
+
             SendOscState([
 
-                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
-                [samplesmainport, '/strip/Samples2Dry/Gain/Mute', 0.0],
-                [samplesmainport, '/strip/Samples4Dry/Gain/Mute', 0.0],
-                [samplesmainport, '/strip/Samples5Dry/Gain/Mute', 0.0],
-                [samplesmainport, '/strip/SamplesMunge/Gain/Mute', 0.0],
-
-                [samplesdelaymungeport, '/strip/Samples2/Gain/Gain%20(dB]/unscaled', -7.0],
+                [samplesmainport, '/strip/Samples3Dry/Gain/Mute', 0.0],
 
             ]),
-
 
             vxorlgars_on,
             vxorlmeuf_off,
@@ -210,8 +228,47 @@ fifty = [
             vxjeannotgars_on,
             vxjeannotmeuf_off,
             vxjeannotdisint_off,
+            vxjeannotvocode_off,
 
             ] >> Discard()
         ],
+    orl >> ProgramFilter(7) >> [ # Refrain - Bouton 7
+        Program(69) >> cseqtrigger,
+        [
+            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
+            SendOSC(slport, '/set', 'tempo', 117),
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
+
+            SendOSC(klickport, '/klick/simple/set_tempo', 117),
+            SendOSC(klickport, '/klick/simple/set_meter', 3, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxx'),
+            SendOSC(klickport, '/klick/metro/start'),
+
+            SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.64925),
+            SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.64925),
+            SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.32222),
+
+            SendOscState([
+
+                [samplesmainport, '/strip/Samples3Dry/Gain/Mute', 0.0],
+
+            ]),
+
+            vxorlgars_on,
+            vxorlmeuf_off,
+            vxorldisint_off,
+            vxorldelay_off,
+            vxorlvocode_off,
+
+            vxjeannotdelay_off,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+
+            ] >> Discard()
+        ],
+
+
 
     ]
