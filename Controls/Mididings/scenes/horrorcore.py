82265727b02b097ac3d3646d54bc855f87747f0a
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-01-28 14:58:16 +0100

    horrorcore: ok

--- Controls/Mididings/scenes/horrorcore.py
+++ Controls/Mididings/scenes/horrorcore.py
@@ -12,7 +12,7 @@ from mididings.extra.osc import SendOSC
 horrorcore = [
     [orl, jeannot] >> Filter(PROGRAM) >> Ctrl(0, 9) >> tapeutapecontrol,
     [orl, jeannot] >> ProgramFilter(1) >> stop, # !!!STOP!!! #
-    orl >> ProgramFilter(2) >> [ # Couplet - Bouton 2
+    orl >> ProgramFilter(2) >> [ # Couplet (orl meuf) - Bouton 2
         Program(65) >> cseqtrigger,
         [
             SendOSC(slport, '/set', 'eighth_per_cycle', 8),
@@ -36,18 +36,151 @@ horrorcore = [
             ]),
 
 
+            vxorlmeuf_on,
             vxorlgars_off,
-            vxorlmeuf_off,
             vxorldisint_off,
             vxorldelay_off,
-            vxorlvocode_on,
+            vxorlvocode_off,
+
+            vxjeannotdelay_off,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+
+            ] >> Discard()
+        ],
+    orl >> ProgramFilter(3) >> [ # Refrain - Bouton 3
+        Program(66) >> cseqtrigger,
+        [
+            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
+            SendOSC(slport, '/set', 'tempo', 200),
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
+
+            SendOSC(klickport, '/klick/simple/set_tempo', 200),
+            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+            SendOSC(klickport, '/klick/metro/start'),
+
+            SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.522388),
+            SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.522388),
+            SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.6296),
+
+
+            SendOscState([
+
+                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
+
+            ]),
+
+
+            vxorlgars_off,
+            vxorlmeuf_on,
+            vxorldisint_off,
+            vxorldelay_on,
+            vxorlvocode_off,
+
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotdelay_on,
+            vxjeannotvocode_off,
+
+            ] >> Discard()
+        ],
+    orl >> ProgramFilter(3) >> [ # Couplet 2 (orl gars, jeannot rabbits) - Bouton 2
+        Program(65) >> cseqtrigger,
+        [
+            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
+            SendOSC(slport, '/set', 'tempo', 200),
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
+
+            SendOSC(klickport, '/klick/simple/set_tempo', 200),
+            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+            SendOSC(klickport, '/klick/metro/start'),
+
+            SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.522388),
+            SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.522388),
+            SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.6296),
+
+
+            SendOscState([
+
+                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
+
+            ]),
+
+
+            vxorlmeuf_off,
+            vxorlgars_on,
+            vxorldisint_off,
+            vxorldelay_off,
+            vxorlvocode_off,
 
             vxjeannotdelay_off,
             vxjeannotgars_off,
             vxjeannotmeuf_on,
             vxjeannotdisint_off,
             vxjeannotvocode_off,
+
             ] >> Discard()
         ],
+    jeannot >> ProgramFilter(2) >> [ # WE IS DA REAL RABBITS (stop milieu couplet) - Bouton 2
+        stop,
+        [
 
+            vxorlmeuf_on,
+            vxorlgars_on,
+            vxorldisint_off,
+            vxorldelay_off,
+            vxorlvocode_off,
+
+            vxjeannotdelay_off,
+            vxjeannotgars_on,
+            vxjeannotmeuf_on,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+
+            ] >> Discard()
+        ],
+    orl >> ProgramFilter(4) >> [ # Outro africa - Bouton 4
+        stop,
+        [
+
+            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
+            SendOSC(slport, '/set', 'tempo', 200),
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
+
+            SendOSC(klickport, '/klick/simple/set_tempo', 200),
+            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+            SendOSC(klickport, '/klick/metro/start'),
+
+            SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.522388),
+            SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.522388),
+            SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.6296),
+
+
+            SendOscState([
+
+                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
+
+            ]),
+
+
+            vxorlmeuf_off,
+            vxorlgars_off,
+            vxorldisint_off,
+            vxorldelay_on,
+            vxorlvocode_on,
+
+            vxjeannotgars_off,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotdelay_on,
+            vxjeannotvocode_on,
+
+            ] >> Discard()
+        ],
     ]
