c9b2074afcee8b09999c56aa0f368f3c1e182420
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-01-28 14:44:25 +0100

    geturfreakon: merge with slowmotium

--- Controls/Mididings/scenes/geturfreakon.py
+++ Controls/Mididings/scenes/geturfreakon.py
@@ -82,7 +82,108 @@ geturfreakon = [
             vxjeannotmeuf_on,
             vxjeannotdisint_off,
             vxjeannotvocode_off,
-            
+
             ] >> Discard()
         ],
+    orl >> ProgramFilter(4) >> [ # SlowMotium (bouclage bass) - Bouton 4
+        Program(65) >> cseqtrigger,
+        [
+            SendOSC(slport, '/set', 'eighth_per_cycle', 4),
+            SendOSC(slport, '/set', 'tempo', 75),
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
+
+            SendOSC(klickport, '/klick/simple/set_tempo', 75),
+            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'xxxx'),
+            SendOSC(klickport, '/klick/metro/start'),
+
+
+            SendOscState([
+
+                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
+
+            ]),
+
+
+            vxorlgars_off,
+            vxorlmeuf_off,
+            vxorldisint_off,
+            vxorldelay_on,
+            vxorlvocode_on,
+
+            vxjeannotdelay_on,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+
+            ] >> Discard()
+        ],
+    orl >> ProgramFilter(5) >> [ # SlowMotium (trig bass) - Bouton 5
+        Program(65) >> cseqtrigger,
+        [
+            SendOSC(slport, '/set', 'eighth_per_cycle', 4),
+            SendOSC(slport, '/set', 'tempo', 75),
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
+
+            SendOSC(slport, '/sl/0/set', 'play_sync', 0),
+            SendOSC(slport, '/sl/0/hit', 'pause_off'),
+            SendOSC(slport, '/sl/0/hit', 'trigger'),
+            SendOSC(slport, '/sl/0/set', 'play_sync', 1),
+
+            SendOSC(klickport, '/klick/simple/set_tempo', 75),
+            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'xxxx'),
+            SendOSC(klickport, '/klick/metro/start'),
+
+
+            SendOscState([
+
+                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
+
+            ]),
+
+
+            vxorlgars_off,
+            vxorlmeuf_off,
+            vxorldisint_off,
+            vxorldelay_on,
+            vxorlvocode_on,
+
+            vxjeannotdelay_on,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+
+            ] >> Discard()
+        ],
+    jeannot >> ProgramFilter(2) >> [ # SlowMotium (vxdelay off) - Bouton 2
+            vxorlgars_off,
+            vxorlmeuf_off,
+            vxorldisint_off,
+            vxorldelay_off,
+            vxorlvocode_on,
+
+            vxjeannotdelay_on,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+        ],
+
+    jeannot >> ProgramFilter(3) >> [ # SlowMotium (vxdelay on) - Bouton 3
+            vxorlgars_off,
+            vxorlmeuf_off,
+            vxorldisint_off,
+            vxorldelay_on,
+            vxorlvocode_on,
+
+            vxjeannotdelay_on,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+        ],
+
     ]
