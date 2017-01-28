727684196e6fca52d4775a508ec5d71eb25c5747
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-01-28 15:02:01 +0100

    wholeworld: ok

--- Controls/Mididings/scenes/wholeworld.py
+++ Controls/Mididings/scenes/wholeworld.py
@@ -12,8 +12,47 @@ from mididings.extra.osc import SendOSC
 wholeworld = [
     [orl, jeannot] >> Filter(PROGRAM) >> Ctrl(0, 5) >> tapeutapecontrol,
     [orl, jeannot] >> ProgramFilter(1) >> stop, # !!!STOP!!! #
-    orl >> ProgramFilter(2) >> [ # Couplet - Bouton 2
+    orl >> ProgramFilter(2) >> [ # Intro cymbaloume - Bouton 2
         Program(65) >> cseqtrigger,
+        [
+            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
+            SendOSC(slport, '/set', 'tempo', 90),
+            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
+
+            SendOSC(klickport, '/klick/simple/set_tempo', 90),
+            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+            SendOSC(klickport, '/klick/metro/start'),
+
+            SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.44776),
+            SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.44776),
+            SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.2222),
+
+
+            SendOscState([
+
+                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
+                [samplesmainport, '/strip/SamplesScape/Gain/Mute', 0.0],
+
+                [samplesscapeport, '/strip/Samples1/Gain/Gain%20(dB)/unscaled', -6.0],
+
+            ]),
+
+            vxorlgars_off,
+            vxorlmeuf_on,
+            vxorldisint_off,
+            vxorldelay_off,
+            vxorlvocode_off,
+
+            vxjeannotdelay_off,
+            vxjeannotgars_off,
+            vxjeannotmeuf_on,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+            ] >> Discard()
+        ],
+    orl >> ProgramFilter(3) >> [ # Couplet - Bouton 3
+        Program(66) >> cseqtrigger,
         [
             SendOSC(slport, '/set', 'eighth_per_cycle', 8),
             SendOSC(slport, '/set', 'tempo', 90),
@@ -50,8 +89,8 @@ wholeworld = [
             vxjeannotvocode_off,
             ] >> Discard()
         ],
-    orl >> ProgramFilter(3) >> [ # Refrain - Bouton 3
-        Program(66) >> cseqtrigger,
+    orl >> ProgramFilter(4) >> [ # Refrain - Bouton 4
+        Program(67) >> cseqtrigger,
         [
             SendOSC(slport, '/set', 'eighth_per_cycle', 8),
             SendOSC(slport, '/set', 'tempo', 90),
@@ -88,47 +127,9 @@ wholeworld = [
             vxjeannotvocode_off,
             ] >> Discard()
         ],
-    orl >> ProgramFilter(4) >> [ # Pont - Bouton 4
-        Program(67) >> cseqtrigger,
-        [
-            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
-            SendOSC(slport, '/set', 'tempo', 90),
-            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
-
-            SendOSC(klickport, '/klick/simple/set_tempo', 90),
-            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
-            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
-            SendOSC(klickport, '/klick/metro/start'),
-
-            SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.44776),
-            SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.44776),
-            SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.2222),
 
 
-            SendOscState([
-
-                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
-                [samplesmainport, '/strip/SamplesScape/Gain/Mute', 0.0],
-
-                [samplesscapeport, '/strip/Samples1/Gain/Gain%20(dB)/unscaled', -6.0],
-
-            ]),
-
-            vxorlgars_off,
-            vxorlmeuf_on,
-            vxorldisint_off,
-            vxorldelay_off,
-            vxorlvocode_off,
-
-            vxjeannotdelay_off,
-            vxjeannotgars_off,
-            vxjeannotmeuf_on,
-            vxjeannotdisint_off,
-            vxjeannotvocode_off,
-            ] >> Discard()
-        ],
-
-    orl >> ProgramFilter(5) >> [ # Pont - Bouton 5
+    orl >> ProgramFilter(5) >> [ # Outro - Bouton 5
         Program(68) >> cseqtrigger,
         [
             SendOSC(slport, '/set', 'eighth_per_cycle', 8),
