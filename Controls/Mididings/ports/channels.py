241a8ca074d6bf696dbe63222fa2d8269c40de6f
MAO7 <mao7@mao-7.(none)>
2017-01-29 09:24:15 +0100

    fix mk2 cc to program transposition and orl midi channel

--- Controls/Mididings/ports/channels.py
+++ Controls/Mididings/ports/channels.py
@@ -2,14 +2,14 @@ from mididings import *
 
 try:
 
-    jeannot = PortFilter('PBMk2In') >> Filter(CTRL) >> CtrlFilter(range(101,109)) >> CtrlValueFilter(127) >> NoteOn(EVENT_CTRL, 127) >> Transpose(-100) >> Program(EVENT_NOTE)
+    jeannot = PortFilter('PBMk2In') >> Filter(CTRL) >> CtrlFilter(range(101,109)) >> CtrlValueFilter(127) >> NoteOn(EVENT_CTRL, 127) >> Transpose(-101) >> Program(EVENT_NOTE)
 
     jeannotKeys = PortFilter('PBMk2In') >> [
         Filter(CTRL) >> ~CtrlFilter(range(101,109)),
         ~Filter(CTRL)
     ]
 
-    orl     = PortFilter('PBCtrlIn') >> ChannelFilter(1)
+    orl     = PortFilter('PBCtrlIn')
 
 except:
 
