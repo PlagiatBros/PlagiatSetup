e4e3247cbf5eb8e926cc1afd933efb695987c345
Wladimir Ohrelianov II <woii@SCstage.sebkhachott.net>
2017-02-09 19:06:37 +0100

    fix syntax

--- Sequences/light/scenes/connassesacem.py
+++ Sequences/light/scenes/connassesacem.py
@@ -4,7 +4,6 @@ sys.path.append("../Controls/Mididings/")
 from ports import *
 
 def connassesacem_1(seq, timer):
-    seq.send('192.168.0.2:4444', '/la'
     seq.send(qlcstopport, 'stop')
     seq.send('/BC/White/Segment/1', 100)
     seq.send('/BJ/White/Segment/1', 100)
