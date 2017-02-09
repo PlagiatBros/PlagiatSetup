3a140aea52b7bebf5a4f34e620d520de2b97cf64
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-02-09 11:32:59 +0100

    getyourfreakon/slowmotium: ok

--- Controls/Mididings/scenes/geturfreakon.py
+++ Controls/Mididings/scenes/geturfreakon.py
@@ -19,21 +19,17 @@ geturfreakon = [
         SendOSC(audioseqport, '/Audioseq/Sequences/Disable', '*')
     ] >> Discard(),
     [orl, jeannot] >> ProgramFilter(1) >> stop, # !!!STOP!!! #
-    orl >> ProgramFilter(2) >> [ # Couplet - Bouton 2
+    orl >> ProgramFilter(2) >> [ # SlowMotium (bouclage bass) - Bouton 2
         Program(65) >> cseqtrigger,
         [
-            SendOSC(slport, '/set', 'eighth_per_cycle', 8),
-            SendOSC(slport, '/set', 'tempo', 200),
+            SendOSC(slport, '/set', 'eighth_per_cycle', 4),
+            SendOSC(slport, '/set', 'tempo', 75),
             SendOSC(slport, '/sl/-1/hit', 'pause_on'),
 
-            SendOSC(klickport, '/klick/simple/set_tempo', 200),
+            SendOSC(klickport, '/klick/simple/set_tempo', 75),
             SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
-            SendOSC(klickport, '/klick/simple/set_pattern', 'Xxxx'),
+            SendOSC(klickport, '/klick/simple/set_pattern', 'xxxx'),
             SendOSC(klickport, '/klick/metro/start'),
-
-            SendOSC(bassmainport, '/strip/BassScapePost/' + scapebpmpath, 0.522388),
-            SendOSC(samplesscapeport, '/strip/SamplesScape/' + scapebpmpath, 0.522388),
-            SendOSC(samplesscapeport, '/strip/VxORLDelayPost/' + delaybpmpath, 0.6296),
 
 
             SendOscState([
@@ -44,17 +40,54 @@ geturfreakon = [
 
 
             vxorlgars_off,
-            vxorlmeuf_on,
+            vxorlmeuf_off,
             vxorldisint_off,
-            vxorldelay_off,
-            vxorlvocode_off,
+            vxorldelay_on,
+            vxorlvocode_on,
 
-            vxjeannotdelay_off,
-            vxjeannotgars_off,
-            vxjeannotmeuf_on,
+            vxjeannotdelay_on,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
             vxjeannotdisint_off,
             vxjeannotvocode_off,
-            ] >> Discard()
+
+            bassdry,
+            bassscape,
+
+            ] >> Discard(),
+        [
+            bassdetunest_off,
+            bassringst_on,
+            bassvibest_on,
+            bassbufferst_on,
+            ]
+        ],
+    jeannot >> ProgramFilter(2) >> [ # SlowMotium (vxdelay off) - Bouton 2
+            vxorlgars_off,
+            vxorlmeuf_off,
+            vxorldisint_off,
+            vxorldelay_off,
+            vxorlvocode_on,
+
+            vxjeannotdelay_on,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
+        ],
+
+    jeannot >> ProgramFilter(3) >> [ # SlowMotium (vxdelay on) - Bouton 3
+            vxorlgars_off,
+            vxorlmeuf_off,
+            vxorldisint_off,
+            vxorldelay_on,
+            vxorlvocode_on,
+
+            vxjeannotdelay_on,
+            vxjeannotgars_on,
+            vxjeannotmeuf_off,
+            vxjeannotdisint_off,
+            vxjeannotvocode_off,
         ],
     orl >> ProgramFilter(3) >> [ # Refrain - Bouton 3
         Program(66) >> cseqtrigger,
@@ -90,107 +123,17 @@ geturfreakon = [
             vxjeannotdisint_off,
             vxjeannotvocode_off,
 
-            ] >> Discard()
-        ],
-    orl >> ProgramFilter(4) >> [ # SlowMotium (bouclage bass) - Bouton 4
-        Program(65) >> cseqtrigger,
+            bassdry,
+            bassscape,
+
+            ] >> Discard(),
         [
-            SendOSC(slport, '/set', 'eighth_per_cycle', 4),
-            SendOSC(slport, '/set', 'tempo', 75),
-            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
-
-            SendOSC(klickport, '/klick/simple/set_tempo', 75),
-            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
-            SendOSC(klickport, '/klick/simple/set_pattern', 'xxxx'),
-            SendOSC(klickport, '/klick/metro/start'),
-
-
-            SendOscState([
-
-                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
-
-            ]),
-
-
-            vxorlgars_off,
-            vxorlmeuf_off,
-            vxorldisint_off,
-            vxorldelay_on,
-            vxorlvocode_on,
-
-            vxjeannotdelay_on,
-            vxjeannotgars_on,
-            vxjeannotmeuf_off,
-            vxjeannotdisint_off,
-            vxjeannotvocode_off,
-
-            ] >> Discard()
-        ],
-    orl >> ProgramFilter(5) >> [ # SlowMotium (trig bass) - Bouton 5
-        Program(65) >> cseqtrigger,
-        [
-            SendOSC(slport, '/set', 'eighth_per_cycle', 4),
-            SendOSC(slport, '/set', 'tempo', 75),
-            SendOSC(slport, '/sl/-1/hit', 'pause_on'),
-
-            SendOSC(slport, '/sl/0/set', 'play_sync', 0),
-            SendOSC(slport, '/sl/0/hit', 'pause_off'),
-            SendOSC(slport, '/sl/0/hit', 'trigger'),
-            SendOSC(slport, '/sl/0/set', 'play_sync', 1),
-
-            SendOSC(klickport, '/klick/simple/set_tempo', 75),
-            SendOSC(klickport, '/klick/simple/set_meter', 4, 4),
-            SendOSC(klickport, '/klick/simple/set_pattern', 'xxxx'),
-            SendOSC(klickport, '/klick/metro/start'),
-
-
-            SendOscState([
-
-                [samplesmainport, '/strip/Samples1Dry/Gain/Mute', 0.0],
-
-            ]),
-
-
-            vxorlgars_off,
-            vxorlmeuf_off,
-            vxorldisint_off,
-            vxorldelay_on,
-            vxorlvocode_on,
-
-            vxjeannotdelay_on,
-            vxjeannotgars_on,
-            vxjeannotmeuf_off,
-            vxjeannotdisint_off,
-            vxjeannotvocode_off,
-
-            ] >> Discard()
-        ],
-    jeannot >> ProgramFilter(2) >> [ # SlowMotium (vxdelay off) - Bouton 2
-            vxorlgars_off,
-            vxorlmeuf_off,
-            vxorldisint_off,
-            vxorldelay_off,
-            vxorlvocode_on,
-
-            vxjeannotdelay_on,
-            vxjeannotgars_on,
-            vxjeannotmeuf_off,
-            vxjeannotdisint_off,
-            vxjeannotvocode_off,
+            bassdetunest_on,
+            bassringst_on,
+            bassvibest_off,
+            bassbufferst_off,
+            ]
         ],
 
-    jeannot >> ProgramFilter(3) >> [ # SlowMotium (vxdelay on) - Bouton 3
-            vxorlgars_off,
-            vxorlmeuf_off,
-            vxorldisint_off,
-            vxorldelay_on,
-            vxorlvocode_on,
-
-            vxjeannotdelay_on,
-            vxjeannotgars_on,
-            vxjeannotmeuf_off,
-            vxjeannotdisint_off,
-            vxjeannotvocode_off,
-        ],
 
     ]
