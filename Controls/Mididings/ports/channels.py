7c4b9306c1a22ef17e9595c6d8775fa18d3f0551
Wladimir Ohrelianov II <woii@SCstage.sebkhachott.net>
2017-06-12 14:36:13 +0200

    jeannot: backup midiroute pour controleur de remplacement

--- Controls/Mididings/ports/channels.py
+++ Controls/Mididings/ports/channels.py
@@ -4,6 +4,13 @@ try:
 
     jeannot = PortFilter('PBMk2In') >> Filter(CTRL) >> CtrlFilter(range(101,109)) >> CtrlValueFilter(127) >> NoteOn(EVENT_CTRL, 127) >> Transpose(-101) >> Program(EVENT_NOTE)
 
+    # jeannot = PortFilter('PBMk2In') >> [ Filter(CTRL) >> CtrlFilter(range(101,109)) >> CtrlValueFilter(127) >> NoteOn(EVENT_CTRL, 127) >> Transpose(-101) >> Program(EVENT_NOTE),
+    # 					 Filter(PROGRAM) >> [
+	# 					ProgramFilter([1,2,3,4]) >> NoteOn(EVENT_PROGRAM, 127) >> Transpose(4) >> Program(EVENT_NOTE),
+	# 					ProgramFilter([5,6,7,8]) >> NoteOn(EVENT_PROGRAM, 127) >> Transpose(-4) >> Program(EVENT_NOTE)
+	# 					]
+	# 				]
+
     jeannotKeys = PortFilter('PBMk2In') >> [
         Filter(CTRL) >> ~CtrlFilter(range(101,109)),
         ~Filter(CTRL)
