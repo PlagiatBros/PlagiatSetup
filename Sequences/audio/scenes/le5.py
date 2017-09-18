3ec2a378edb1406bcb1b85b452715d64c033fdd4
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-09-18 15:21:00 +0200

    le5: fix parentheses en trop

--- Sequences/audio/scenes/le5.py
+++ Sequences/audio/scenes/le5.py
@@ -23,9 +23,9 @@ def le5_louboutin_close(sequencer, timer):
     t = timer.time()
 
     # vxorldelay_off
-    sequencer.send((vxorlpreport, '/strip/VxORLDelayPre/Gain/Mute', 1.0)
-    sequencer.send((vxorlpostport, '/strip/VxORLDelayPost/Gain/Mute', 1.0)
-    sequencer.send((surfaceorlport, '/vxorl', 'delay', 0)
+    sequencer.send(vxorlpreport, '/strip/VxORLDelayPre/Gain/Mute', 1.0)
+    sequencer.send(vxorlpostport, '/strip/VxORLDelayPost/Gain/Mute', 1.0)
+    sequencer.send(surfaceorlport, '/vxorl', 'delay', 0)
 
     # mute  SAMPLES
     sequencer.animate([samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 1., .25, 1, 'beat', framerate=50, timestamp=t)
@@ -34,9 +34,9 @@ def le5_louboutin_close(sequencer, timer):
     t = timer.time()
 
     # vxorldelay_on
-    sequencer.send((vxorlpreport, '/strip/VxORLDelayPre/Gain/Mute', 0.0)
-    sequencer.send((vxorlpostport, '/strip/VxORLDelayPost/Gain/Mute', 0.0)
-    sequencer.send((surfaceorlport, '/vxorl', 'delay', 1)
+    sequencer.send(vxorlpreport, '/strip/VxORLDelayPre/Gain/Mute', 0.0)
+    sequencer.send(vxorlpostport, '/strip/VxORLDelayPost/Gain/Mute', 0.0)
+    sequencer.send(surfaceorlport, '/vxorl', 'delay', 1)
 
     # unmute  SAMPLES
     sequencer.animate([samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 0.25, 1., 0.75, 'beat', framerate=50, timestamp=t)
