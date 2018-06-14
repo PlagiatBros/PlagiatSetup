243647f3cab5add211357a9b3b6b21435990428f
Aurélien Roux <orl@ammd.net>
2018-06-14 13:02:11 +0200

    randomize shit and butter

--- Sequences/light/scenes/fifty.py
+++ Sequences/light/scenes/fifty.py
@@ -20,17 +20,22 @@ def fifty_tea(seq, timer):
       timer.wait(0.25, 'beat')
   seq.send('/pyta/slide/alpha', colos, 0.1)
 
+
+butters = ['Butter', 'BuTtEr', 'butter', 'BUTTER', 'bUtTeR', 'BuRgEr' , 'BeTTeR']
+shits = ['Shit', '$hIt', '$HIT', '$hit', '$hibre', '$HiT', 'ChIt', 'ChIbrr']
   
 def fifty_butter(seq, timer):
   i = random.randomint(0,2)
-  seq.send('/pyta/text', i, "BuTtEr"),
+  j = random.randomint(0,7)
+  seq.send('/pyta/text', i, butters[j]),
   seq.send('/pyta/text/visible', i, 1)
   timer.wait(2, 'beat)
   seq.send('/pyta/text/visible', i, 0)
              
 def fifty_shit(seq, timer):
   i = random.randomint(0,2)
-  seq.send('/pyta/text', i, "$hIt"),
+  j = random.randomint(0,8)
+  seq.send('/pyta/text', i, shits[j]),
   seq.send('/pyta/text/visible', i, 1)
   timer.wait(2, 'beat)
   seq.send('/pyta/text/visible', i, 0)        
