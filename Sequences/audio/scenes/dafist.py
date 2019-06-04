1342d39736307c65b83471c2a2e0f8d479fa07d4
bordun <jean-emmanuel.doucet@groolot.net>
2019-06-04 18:33:06 +0200

    ajout keyboard pitchbend dans sequences audio

--- Sequences/audio/scenes/dafist.py
+++ Sequences/audio/scenes/dafist.py
@@ -6,6 +6,7 @@ from ports import *
 def dafist_outro_filter_close(sequencer, timer):
 
     sequencer.animate([samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 1, .25, 0.75, 'beat', framerate=50)
+    sequencer.animate([samplesmainport, '/strip/Keyboards/AM%20pitchshifter/Pitch%20shift/unscaled'], 1, .25, 0.75, 'beat', framerate=50)
     sequencer.send(slport, '/sl/7/hit', 'mute_on')
     timer.wait( 1, 'bites')
 
@@ -14,7 +15,9 @@ def dafist_outro_filter_close(sequencer, timer):
     timer.wait(.25, 'bites')
     sequencer.send(slport, '/sl/7/hit', 'mute_off')
     sequencer.animate([samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled'], .25, 1, 0.75, 'beat', framerate=50)
+    sequencer.animate([samplesmainport, '/strip/Keyboards/AM%20pitchshifter/Pitch%20shift/unscaled'], .25, 1, 0.75, 'beat', framerate=50)
 
 def dafist_filter_reset(sequencer, timer):
     sequencer.send(slport, '/sl/7/hit', 'mute_off')
     sequencer.send(samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled', 1.)
+    sequencer.send(samplesmainport, '/strip/Keyboards/AM%20pitchshifter/Pitch%20shift/unscaled', 1.)
--- Sequences/audio/scenes/le5.py
+++ Sequences/audio/scenes/le5.py
@@ -8,6 +8,7 @@ def le5_refrain_cutdown_close(sequencer, timer):
     t = timer.time()
 
     sequencer.animate([samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 1, .25, 1, 'beat', framerate=50, timestamp=t)
+    sequencer.animate([samplesmainport, '/strip/Keyboards/AM%20pitchshifter/Pitch%20shift/unscaled'], 1, .25, 1, 'beat', framerate=50, timestamp=t)
     sequencer.animate([vxmainport, '/strip/VxORLMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 1, .25, 1, 'beat', framerate=50, timestamp=t)
     sequencer.animate([vxmainport, '/strip/VxJeannotMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 1, .25, 1, 'beat', framerate=50, timestamp=t)
     sequencer.animate([bassmainport, '/strip/BassMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 1, .25, 1, 'beat', framerate=50, timestamp=t)
@@ -15,6 +16,7 @@ def le5_refrain_cutdown_close(sequencer, timer):
     timer.wait(3, 'bites')
 
     sequencer.send(samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled', 1.)
+    sequencer.send(samplesmainport, '/strip/Keyboards/AM%20pitchshifter/Pitch%20shift/unscaled', 1.)
     sequencer.send(vxmainport, '/strip/VxJeannotMain/AM%20pitchshifter/Pitch%20shift/unscaled', 1.)
     sequencer.send(vxmainport, '/strip/VxORLMain/AM%20pitchshifter/Pitch%20shift/unscaled', 1.)
     sequencer.send(bassmainport, '/strip/BassMain/AM%20pitchshifter/Pitch%20shift/unscaled', 1.)
@@ -29,6 +31,7 @@ def le5_louboutin_close(sequencer, timer):
 
     # mute  SAMPLES
     sequencer.animate([samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 1., .25, 1, 'beat', framerate=50, timestamp=t)
+    sequencer.animate([samplesmainport, '/strip/Keyboards/AM%20pitchshifter/Pitch%20shift/unscaled'], 1., .25, 1, 'beat', framerate=50, timestamp=t)
 
     timer.wait(3.25, 'bites')
     t = timer.time()
@@ -40,3 +43,4 @@ def le5_louboutin_close(sequencer, timer):
 
     # unmute  SAMPLES
     sequencer.animate([samplesmainport, '/strip/SamplesMain/AM%20pitchshifter/Pitch%20shift/unscaled'], 0.25, 1., 0.75, 'beat', framerate=50, timestamp=t)
+    sequencer.animate([samplesmainport, '/strip/Keyboards/AM%20pitchshifter/Pitch%20shift/unscaled'], 0.25, 1., 0.75, 'beat', framerate=50, timestamp=t)
