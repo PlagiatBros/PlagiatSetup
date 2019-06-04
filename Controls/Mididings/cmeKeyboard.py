7e41f4dcd7e2479390e4dc2a253d43899ebd415a
bordun <jean-emmanuel.doucet@groolot.net>
2019-06-04 18:30:54 +0200

    ajout keyboards sur le filtre global

--- Controls/Mididings/cmeKeyboard.py
+++ Controls/Mididings/cmeKeyboard.py
@@ -120,7 +120,10 @@ run(
     },
     control = [
         Filter(CTRL) >> [
-            CtrlFilter(110) >>  SendOSC(samplesmainport, '/strip/SamplesMain/Calf%20Filter/Frequency/unscaled', lambda ev: 20000. * pow(10, ((-log10(71/20000.))*ev.value) / 127. + log10(71/20000.))),
+            CtrlFilter(110) >>  [
+				SendOSC(samplesmainport, '/strip/SamplesMain/Calf%20Filter/Frequency/unscaled', lambda ev: 20000. * pow(10, ((-log10(71/20000.))*ev.value) / 127. + log10(71/20000.)))
+				SendOSC(samplesmainport, '/strip/Keyboards/Calf%20Filter/Frequency/unscaled', lambda ev: 20000. * pow(10, ((-log10(71/20000.))*ev.value) / 127. + log10(71/20000.)))
+				],
             CtrlFilter(75) >> gatecancel,
             CtrlFilter(1) >> Call(glitch),
         ] >> Discard(),
