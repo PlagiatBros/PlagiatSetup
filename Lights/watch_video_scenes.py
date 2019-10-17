729a921e02738c3ec1dabedf3d543d98961c7d8e
bordun <jean-emmanuel.doucet@groolot.net>
2019-10-17 11:19:30 +0200

    video file watch: load at init

--- Lights/watch_video_scenes.py
+++ Lights/watch_video_scenes.py
@@ -1,14 +1,15 @@
 import liblo
 
 # watcher
-def restart(event=None):
+def reload(event=None):
     liblo.send('osc.udp://127.0.0.1:5555', '/pyta/scene_import', '../Scenes/*/*.video')
     liblo.send('osc.udp://127.0.0.1:5556', '/pyta/scene_import', '../Scenes/*/*.video')
 
+reload()
 
 import sys, os, pyinotify
 watcher = pyinotify.WatchManager()
-notifier = pyinotify.Notifier(watcher, restart)
+notifier = pyinotify.Notifier(watcher, reload)
 base_dir = os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__))
 watcher.add_watch('./Scenes', pyinotify.IN_MODIFY | pyinotify.IN_CREATE, rec=True)
 print('Monitoring video scene files... (press ctrl+x to exit)')
