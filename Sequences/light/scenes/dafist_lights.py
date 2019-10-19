345859be6d8f32f03d8fcb2c4b7c04258d9a2345
Wladimir Ohrelianov II <woii@SCstage.sebkhachott.net>
2019-10-19 22:14:44 +0200

    Mise à jour linéarisation

--- Sequences/light/scenes/dafist_lights.py
+++ Sequences/light/scenes/dafist_lights.py
@@ -8,29 +8,29 @@ from scene_functions import *
 
 def dafist_intro_fixe(seq, timer):
     seq.send(qlcport, '/ProcheCour/Red/Segment/{1,2,3,8}', 255)
-    seq.send(qlcport, '/ProcheCour/Green/Segment/{1,2,3,8}', 140)
-    seq.send(qlcport, '/ProcheCour/Blue/Segment/{1,2,3,8}', 180)
+    seq.send(qlcport, '/ProcheCour/Green/Segment/{1,2,3,8}', 135)#140)
+    seq.send(qlcport, '/ProcheCour/Blue/Segment/{1,2,3,8}', 161)#180)
 
     seq.send(qlcport, '/ProcheJardin/Red/Segment/{1,2,3,8}', 255)
-    seq.send(qlcport, '/ProcheJardin/Green/Segment/{1,2,3,8}', 100)
-    seq.send(qlcport, '/ProcheJardin/Blue/Segment/{1,2,3,8}', 30)
+    seq.send(qlcport, '/ProcheJardin/Green/Segment/{1,2,3,8}', 109)#100)
+    seq.send(qlcport, '/ProcheJardin/Blue/Segment/{1,2,3,8}', 56)#30)
 
-    seq.send(qlcport, '/LointainCour/Red/Segment/{2,3,8}', 30)
-    seq.send(qlcport, '/LointainCour/Green/Segment/{2,3,8}', 18)
-    seq.send(qlcport, '/LointainCour/Blue/Segment/{2,3,8}', 22)
+    seq.send(qlcport, '/LointainCour/Red/Segment/{2,3,8}', 56)#30)
+    seq.send(qlcport, '/LointainCour/Green/Segment/{2,3,8}', 43)#18)
+    seq.send(qlcport, '/LointainCour/Blue/Segment/{2,3,8}', 48)#22)
 
-    seq.send(qlcport, '/LointainJardin/Red/Segment/{2,3,8}', 30)
-    seq.send(qlcport, '/LointainJardin/Green/Segment/{2,3,8}', 12)
-    seq.send(qlcport, '/LointainJardin/Blue/Segment/{2,3,8}', 3)
+    seq.send(qlcport, '/LointainJardin/Red/Segment/{2,3,8}', 56)#30)
+    seq.send(qlcport, '/LointainJardin/Green/Segment/{2,3,8}', 35)#12)
+    seq.send(qlcport, '/LointainJardin/Blue/Segment/{2,3,8}', 17)#3)
 
 def dafist_battements(seq,timer):
     seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiCour/Red/Segment/{4,5,6,7}', 255, 0, 0.25*seq.bpm/60, 0.01])
-    seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiCour/Green/Segment/{4,5,6,7}', 140, 0, 0.25*seq.bpm/60, 0.01])
-    seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiCour/Blue/Segment/{4,5,6,7}', 180, 0, 0.25*seq.bpm/60, 0.01])
+    seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiCour/Green/Segment/{4,5,6,7}', 135, 0, 0.25*seq.bpm/60, 0.01])
+    seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiCour/Blue/Segment/{4,5,6,7}', 161, 0, 0.25*seq.bpm/60, 0.01])
 
     seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiJardin/Red/Segment/{4,5,6,7}', 255, 0, 0.25*seq.bpm/60, 0.01])
-    seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiJardin/Green/Segment/{4,5,6,7}', 100, 0, 0.25*seq.bpm/60, 0.01])
-    seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiJardin/Blue/Segment/{4,5,6,7}', 30, 0, 0.25*seq.bpm/60, 0.01])
+    seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiJardin/Green/Segment/{4,5,6,7}', 109, 0, 0.25*seq.bpm/60, 0.01])
+    seq.scene_run_subscene(eased_fade, [seq, timer, '/TuttiJardin/Blue/Segment/{4,5,6,7}', 56, 0, 0.25*seq.bpm/60, 0.01])
 
 def dafist_battements_intensity(seq,timer):
     while True:
@@ -58,15 +58,15 @@ def dafist_refrain_montee(seq,timer):
 
                        
 def dafist_couplet_firstpart_fixe(seq,timer):
-    seq.send(qlcport, '/ProcheCour/Red/Segment/{1,8}', 105)
-    seq.send(qlcport, '/ProcheCour/Green/Segment/{1,8}', 140)
-    seq.send(qlcport, '/ProcheCour/Blue/Segment/{1,8}', 100)
+    seq.send(qlcport, '/ProcheCour/Red/Segment/{1,8}', 113)#105)
+    seq.send(qlcport, '/ProcheCour/Green/Segment/{1,8}', 135)#140)
+    seq.send(qlcport, '/ProcheCour/Blue/Segment/{1,8}', 109)#100)
 
-    seq.send(qlcport, '/ProcheJardin/Red/Segment/{4,8}', 35)
-    seq.send(qlcport, '/ProcheJardin/Green/Segment/{4,8}', 80)
-    seq.send(qlcport, '/ProcheJardin/Blue/Segment/{4,8}', 50)
+    seq.send(qlcport, '/ProcheJardin/Red/Segment/{4,8}', 61)#35)
+    seq.send(qlcport, '/ProcheJardin/Green/Segment/{4,8}', 96)#80)
+    seq.send(qlcport, '/ProcheJardin/Blue/Segment/{4,8}', 74)#50)
 
-    crepitement(seq, timer, [['LointainCour', 'Red', '{4,5,6}', 100, 105], ['LointainCour', 'Green', '{4,5,6}', 140, 150],['LointainCour', 'Blue', '{4,5,6}', 96, 110],['LointainJardin', 'Red', '{4,5,6}', 100, 105], ['LointainJardin', 'Green', '{4,5,6}', 140, 150],['LointainJardin', 'Blue', '{4,5,6}', 96, 110]])
+    crepitement(seq, timer, [['LointainCour', 'Red', '{4,5,6}', 109, 113], ['LointainCour', 'Green', '{4,5,6}', 135, 141],['LointainCour', 'Blue', '{4,5,6}', 107, 116],['LointainJardin', 'Red', '{4,5,6}', 109, 113], ['LointainJardin', 'Green', '{4,5,6}', 135, 141],['LointainJardin', 'Blue', '{4,5,6}', 107, 116]])
 
 def dafist_couplet_oscillation_up2cour(seq,timer):
     seq.animate([qlcport, '/damper', '/LointainCour/Red/Segment/{4,5,6}'], 0.01, 1, 8, 'b')
@@ -87,9 +87,9 @@ def dafist_couplet_oscillation_up2jardin(seq,timer):
     seq.animate([qlcport, '/damper', '/LointainCour/Green/Segment/{4,5,6}'], 1, 0.01, 8, 'b')
 
 def dafist_couplet_secondpart_fixe(seq,timer):
-    seq.send(qlcport, '/TuttiLointain/Blue/Segment/8', 200)
-    seq.send(qlcport, '/LointainCour/Blue/Segment/{1,7}', 130)
-    seq.send(qlcport, '/ProcheJardin/White/Segment/{1,2}', 200)
+    seq.send(qlcport, '/TuttiLointain/Blue/Segment/8', 176)#200)
+    seq.send(qlcport, '/LointainCour/Blue/Segment/{1,7}', 129)#130)
+    seq.send(qlcport, '/ProcheJardin/White/Segment/{1,2}', 176)#200)
 
     seq.send(qlcport, '/damper', '/LointainJardin/Red/Segment/{4,5,6}', 0)
     seq.send(qlcport, '/damper', '/LointainJardin/Blue/Segment/{4,5,6}', 0)
@@ -100,8 +100,8 @@ def dafist_couplet_secondpart_fixe(seq,timer):
     seq.send(qlcport, '/damper', '/LointainCour/Green/Segment/{4,5,6}', 0)
 
 def dafist_couplet_thirdpart_fixe(seq,timer):
-    seq.send(qlcport, '/TuttiProche/Blue/Segment/8', 200)
-    seq.send(qlcport, '/LointainCour/Blue/Segment/{2,6}', 130)
+    seq.send(qlcport, '/TuttiProche/Blue/Segment/8', 176)#200)
+    seq.send(qlcport, '/LointainCour/Blue/Segment/{2,6}', 129)#130)
 
 
 transe_bass = 0
@@ -110,14 +110,14 @@ def dafist_couplet_thirdpart_flash(seq,timer):
     transe_bass = 0 # sécu si jamais transe jouée en balances
     seq.scene_run_subscene(eased_fade,[seq, timer, '/Tutti/Blue/Segment/{1,4,5,7}', 0, 255, 0.1*seq.bpm/60, 0.01])
     timer.wait(0.45, 'b')
-    eased_fade(seq, timer, '/Tutti/Green/Segment/{1,4,5,7}', 160, 255, 0.1*seq.bpm/60, 0.01)
+    eased_fade(seq, timer, '/Tutti/Green/Segment/{1,4,5,7}', 148, 255, 0.1*seq.bpm/60, 0.01)
     eased_fade(seq, timer, '/Tutti/{Blue,Green}/Segment/{1,4,5,7}', 255, 0, 0.1*seq.bpm/60, 0.01)
 
 
     
 def dafist_transe_intro(seq,timer):
     global transe_bass
-    crepitement(seq, timer, [['TuttiLointain', 'Red', '{1,4,5,8}', 85, 104], ['TuttiLointain', 'Green', '{1,4,5,8}', 125, 140], ['TuttiLointain', 'Blue', '{1,4,5,8}', 85, 100]])
+    crepitement(seq, timer, [['TuttiLointain', 'Red', '{1,4,5,8}', 99, 112], ['TuttiLointain', 'Green', '{1,4,5,8}', 125, 135], ['TuttiLointain', 'Blue', '{1,4,5,8}', 99, 109]])
 
 
 transe_step = 0
@@ -152,5 +152,5 @@ def dafist_transe_tot(seq,timer):
     bar_chase(seq, timer, 'TuttiLointain', [[255, 255, 255]], ['bt'], [0.12, 'b'])
 
 def dafist_transe_tota(seq,timer):
-    bar_chase(seq, timer, 'TuttiProche', [[30, 120, 140]], ['bt'], [0.25, 'b'])
+    bar_chase(seq, timer, 'TuttiProche', [[56, 122, 135]], ['bt'], [0.25, 'b'])
 
