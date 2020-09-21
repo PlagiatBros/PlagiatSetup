f0f71a74d7148272b21f680e2edf451918199195
bordun <jean-emmanuel.doucet@groolot.net>
2020-09-21 18:23:39 +0200

    gui: disable gpu & fix loop lock

--- Scripts/launch_gUI.sh
+++ Scripts/launch_gUI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Plagiat/Stage/Interfaces
 python ./nonState.py &
-open-stage-control -l orl.json -c orl-custom-module.js -p 11000
+open-stage-control -l orl.json -c orl-custom-module.js -p 11000 --disable-gpu
