b505fa015d3593085627e5eae38f1001a698c6cb
bordun <jean-emmanuel.doucet@groolot.net>
2020-12-07 11:25:18 +0100

    sah y è le synth 'yé cassé comme faut

--- Controls/Mididings/microTonalPitcher.py
+++ Controls/Mididings/microTonalPitcher.py
@@ -1,5 +1,6 @@
 # coding=utf8
 from mididings import *
+from mididings.event import PitchbendEvent
 from mididings.extra.inotify import AutoRestart
 from liblo import ServerThread
 
@@ -12,15 +13,16 @@ config(
 
 from ports import *
 
-hook(
-    AutoRestart()
-)
+# hook(
+#     AutoRestart()
+# )
 
 monosynth_pitch = [0 for i in range(12)]
 manual_pitch = [0, 0, 0]
 
 
 def set_microtonal(path, args):
+    global monosynth_pitch
     monosynth_pitch = [8192. * t / 2 for t in args]
     print('monosynth pitch: %s' % monosynth_pitch)
 
@@ -29,17 +31,18 @@ server = ServerThread(monosynthpitcherport)
 server.add_method('/monosynth/pitch', None, set_microtonal)
 server.start()
 
-def applyMicrotonal(ev, input):
+def applyMicrotonal(ev):
     note = ev.note % 12
-    port = ev.port
-    return [ev, Pitchbend(monosynth_pitch[note] + manual_pitch[port])]
+    port = ev.port - 1
+    print(int(monosynth_pitch[note] + manual_pitch[port]))
+    return [PitchbendEvent(ev.port, ev.channel, int(monosynth_pitch[note] + manual_pitch[port])), ev]
 
-def storePitchwheel(ev, input):
-    port = ev.port
+def storePitchwheel(ev):
+    port = ev.port - 1
     manual_pitch[port] = ev.value
-    return Discard()
+    return ev
 
-run(
+run([
     Filter(NOTEON) >> Process(applyMicrotonal),
-    Filter(PITCHBEND) >> Process(storePitchwheel)
-)
+    Filter(PITCHBEND) >> Process(storePitchwheel) >> Discard()
+])
